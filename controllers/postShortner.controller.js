b14ca38a97d1cab3b5ba13c6613d85fda81aa918
Deepak <sinhadeepak619@gmail.com>
2025-02-25 15:01:29 +0530

    updated url-shortener using drizzle

--- controllers/postShortner.controller.js
+++ controllers/postShortner.controller.js
@@ -1,55 +1,66 @@
 // import { readFile } from "fs/promises";
 // import path from "path";
 import crypto from "crypto";
-import { loadLinks, saveLinks } from "../models/shortener.model.js";
+// import { loadLinks, saveLinks } from "../models/shortener.model.js";
+import {
+  getAllShortLinks,
+  getShortLinkByShortCode,
+  insertShortLink,
+} from "../services/shortener.services.js";
 
 export const getShortnerPage = async (req, res) => {
-    try {
-        // const file = await readFile(path.join("views", "index.html"));
-        const links = await loadLinks();
+  try {
+    // const file = await readFile(path.join("views", "index.html"));
+    // const links = await loadLinks();
 
-        return res.render("index.ejs", { links, host: req.host});
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send("Internal server error");
-    }
+    const links = await getAllShortLinks();
+
+    return res.render("index.ejs", { links, host: req.host });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
 };
-
 
 export const postURLShortner = async (req, res) => {
-    try {
-        const { url, shortcode } = req.body;
-        const finalShortCode = shortcode || crypto.randomBytes(4).toString("hex");
+  try {
+    const { url, shortcode } = req.body;
+    const finalShortCode = shortcode || crypto.randomBytes(4).toString("hex");
 
-        const links = await loadLinks();
+    // const links = await loadLinks();
 
-        if (links[finalShortCode]) {
-            res.status(400).send("Short code already exists. Please choose another.");
-        }
+    const link = await getShortLinkByShortCode(finalShortCode);
 
-        links[finalShortCode] = url;
-    
-        await saveLinks(links);
-        return res.redirect("/");
-
-    } catch (error) {
-        console.log(error);
-        res.status(200).send("Server error: " + error.message);
+    if (link) {
+      res.status(400).send("Short code already exists. Please choose another.");
     }
+
+    // link[finalShortCode] = url;
+
+    // await saveLinks(link);
+
+    await insertShortLink({ url, finalShortCode });
+
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.status(200).send("Server error: " + error.message);
+  }
 };
 
-
 export const redirectToShort = async (req, res) => {
-    try {
-        const { shortCode } = req.params;
-        const links = await loadLinks();
+  try {
+    const { shortCode } = req.params;
+    // const links = await loadLinks();
+    const link = await getShortLinkByShortCode(shortCode);
+    console.log("🚀 ~ redirectToShortLink ~ link:", link);
 
-        if(!links[shortCode]) {
-            return res.status(400).send("404 error occured");
-        }
-
-        return res.redirect(links[shortCode]);
-    } catch (error) {
-        return res.status(500).send("Internal server error");
+    if (!link) {
+      return res.status(400).send("404 error occured");
     }
+
+    return res.redirect(link.url);
+  } catch (error) {
+    return res.status(500).send("Internal server error");
+  }
 };
---
+++ services/shortener.services.js
@@ -0,0 +1,20 @@
+import { eq } from "drizzle-orm";
+import { db } from "../config/db.js";
+import { shortLink } from "../drizzle/schema.js";
+
+export const getAllShortLinks = async () => {
+  return await db.select().from(shortLink);
+};
+
+export const getShortLinkByShortCode = async (shortCode) => {
+  const [result] = await db
+    .select()
+    .from(shortLink)
+    .where(eq(shortLink.shortCode, shortCode));
+
+  return result;
+};
+
+export const insertShortLink = async ({ url, finalShortCode }) => {
+  await db.insert(shortLink).values({ url, shortCode: finalShortCode });
+};
