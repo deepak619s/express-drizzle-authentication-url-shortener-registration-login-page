a827e2adcfa1f7ef8419c7dce474afb70c89b281
Deepak <sinhadeepak619@gmail.com>
2025-03-14 08:42:08 +0530

    Refreshing Access Token When It Expires (Session + JWT)

--- services/auth.services.js
+++ services/auth.services.js
@@ -52,9 +52,9 @@ export const createSession = async (userId, { ip, userAgent }) => {
 };
 
 // createAccessToken :-
-export const createAccessToken = async ({ id, name, email, sessionId }) => {
+export const createAccessToken = ({ id, name, email, sessionId }) => {
   return jwt.sign({ id, name, email, sessionId }, process.env.JWT_SECRET, {
-    expiresIn: ACCESS_TOKEN_EXPIRY / MILLISECONDS_PER_SECOND, //   expiresIn: "15m",
+    expiresIn: ACCESS_TOKEN_EXPIRY / MILLISECONDS_PER_SECOND,
   });
 };
 
@@ -68,4 +68,58 @@ export const createRefreshToken = (sessionId) => {
 // verifyJWTToken :-
 export const verifyJWTToken = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+// findSessionById :-
+export const findSessionById = async (sessionId) => {
+  const [session] = await db
+    .select()
+    .from(sessionsTable)
+    .where(eq(sessionsTable.id, sessionId));
+
+  return session;
+};
+
+// finsUserById :-
+export const finsUserById = async (userId) => {
+  const [user] = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.id, userId));
+
+  return user;
+};
+
+// refreshTokens :-
+export const refreshTokens = async (refreshToken) => {
+  try {
+    const decodedToken = verifyJWTToken(refreshToken);
+    const currentSession = await findSessionById(decodedToken.sessionId);
+
+    if (!currentSession || !currentSession.valid) {
+      throw new Error("Invalid Session");
+    }
+
+    const user = await finsUserById(currentSession.userId);
+
+    if (!user) throw new Error("Invalid User");
+
+    const userInfo = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      sessionId: currentSession.id,
+    };
+
+    const newAccessToken = createAccessToken(userInfo);
+    const newRefreshToken = createRefreshToken(currentSession.id);
+
+    return {
+      newAccessToken,
+      newRefreshToken,
+      user: userInfo,
+    };
+  } catch (error) {
+    console.log(error.message);
+  }
 };
