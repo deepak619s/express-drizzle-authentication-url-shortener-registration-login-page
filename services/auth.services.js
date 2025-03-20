828ae15d5eae54796398af612505367e3539e90e
Deepak <sinhadeepak619@gmail.com>
2025-03-20 09:29:11 +0530

    How Email Verification Works

--- services/auth.services.js
+++ services/auth.services.js
@@ -108,6 +108,7 @@ export const refreshTokens = async (refreshToken) => {
       id: user.id,
       name: user.name,
       email: user.email,
+      isEmailValid: user.isEmailValid,
       sessionId: currentSession.id,
     };
 
@@ -141,6 +142,7 @@ export const authenticateUser = async ({ req, res, user, name, email }) => {
     id: user.id,
     name: user.name || name,
     email: user.email || email,
+    isEmailValid: false,
     sessionId: session.id,
   });
 
